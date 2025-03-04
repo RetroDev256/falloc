5d270341fad36e1acd82bafddff3fa781285a224
Eric Petersen <ericpetersen3.14@gmail.com>
2025-03-04 10:34:16 -0700

    omg it works

--- src/Falloc.zig
+++ src/Falloc.zig
@@ -1,1 +1,61 @@
-// Hello
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const Alignment = std.mem.Alignment;
+
+// TODO: manage context in an "always allocated" mapping to a file
+// This context will allow things to be resized and freed and stuff
+
+// TODO: ensure alloc properly aligns to more than just page size
+
+pub const allocator: Allocator = .{
+    .ptr = undefined,
+    .vtable = &.{
+        .alloc = alloc,
+        .resize = resize,
+        .remap = remap,
+        .free = free,
+    },
+};
+
+fn alloc(_: *anyopaque, len: usize, _: Alignment, _: usize) ?[*]u8 {
+    const fd = tmpfile() catch return null;
+    errdefer std.posix.close(fd);
+    std.posix.ftruncate(fd, @intCast(len)) catch return null;
+    const mapping = std.posix.mmap(
+        null,
+        len,
+        std.posix.PROT.WRITE | std.posix.PROT.READ | std.posix.PROT.SEM,
+        .{ .UNINITIALIZED = true, .NORESERVE = true, .TYPE = .SHARED },
+        fd,
+        0,
+    ) catch return null;
+    return mapping.ptr;
+}
+
+fn resize(_: *anyopaque, memory: []u8, alignment: Alignment, new_len: usize, ret_addr: usize) bool {
+    _ = .{ memory, alignment, new_len, ret_addr };
+    return false;
+}
+
+fn remap(_: *anyopaque, memory: []u8, alignment: Alignment, new_len: usize, ret_addr: usize) ?[*]u8 {
+    _ = .{ memory, alignment, new_len, ret_addr };
+    return null;
+}
+
+fn free(_: *anyopaque, memory: []u8, alignment: Alignment, ret_addr: usize) void {
+    _ = .{ memory, alignment, ret_addr };
+    // well frick
+}
+
+fn tmpfile() !std.posix.fd_t {
+    const id = std.crypto.random.int(u128);
+    const path = "/tmp/" ++ std.fmt.hex(id);
+    const fd = try std.posix.open(path, .{
+        .CREAT = true,
+        .ACCMODE = .RDWR,
+        .EXCL = true,
+    }, 0o600);
+    errdefer std.posix.close(fd);
+    try std.posix.unlink(path);
+    return fd;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const Falloc = @import("Falloc");
 
 pub fn main() !void {
-    //
+    const gpa: Allocator = Falloc.allocator;
+    const mem = try gpa.alloc(u8, 256 * 1024 * 1024 * 1024);
+    const hello = "Hello, World!\n";
+    @memcpy(mem[0..hello.len], hello);
+    std.debug.print("{s}", .{mem[0..hello.len]});
 }
