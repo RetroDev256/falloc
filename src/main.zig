82c495bb7332a83f7de74193311a14140989a1da
Eric Petersen <ericpetersen3.14@gmail.com>
2025-03-04 21:02:17 -0700

    comments

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,11 @@ const Falloc = @import("Falloc");
 
 pub fn main() !void {
     const gpa: Allocator = Falloc.allocator;
-    const mem = try gpa.alloc(u8, 1 << 30); // 1 GiB
+    // 256 GiB (may take a while, look at disk IO for your progress bar lol)
+    const mem = try gpa.alloc(u8, 1 << 38);
     defer gpa.free(mem);
+
+    // In a real-world use case scenario, this would be better utilized.
     const hello = "Hello, World!\n";
     @memcpy(mem[0..hello.len], hello);
     std.debug.print("{s}", .{mem[0..hello.len]});
