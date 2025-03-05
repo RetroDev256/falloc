6a527a05b6d112cd6e2dadbc63f19f81e9a0eb2a
Eric Petersen <ericpetersen3.14@gmail.com>
2025-03-04 20:49:08 -0700

    tighten everything

--- src/Falloc.zig
+++ src/Falloc.zig
@@ -3,8 +3,8 @@ const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
 const Alignment = std.mem.Alignment;
 
-// TODO: manage context in an "always allocated" mapping to a file
-// This context will allow things to be resized and freed and stuff
+const PROT = std.posix.PROT;
+const MAP = std.posix.MAP;
 
 pub const allocator: Allocator = .{
     .ptr = undefined,
@@ -20,173 +20,123 @@ pub const allocator: Allocator = .{
 // It is always at the end of the allocated memory
 const Footer = struct {
     origin: [*]align(std.heap.page_size_min) u8,
-    map_len: usize,
-    file_len: usize, // TODO: see if we can merge the two lens
     fd: std.posix.fd_t,
-};
-const footer_a: Alignment = .fromByteUnits(@alignOf(Footer));
 
-/// Return a pointer to `len` bytes with specified `alignment`, or return
-/// `null` indicating the allocation failed.
-///
-/// `ret_addr` is optionally provided as the first return address of the
-/// allocation call stack. If the value is `0` it means no return address
-/// has been provided.
+    const footer_align: Alignment = .fromByteUnits(@alignOf(Footer));
+
+    // Get a pointer to where the footer is / should be
+    fn acquire(data_ptr: [*]u8, data_len: usize) *Footer {
+        const aligned_addr: usize = @intFromPtr(data_ptr);
+        const footer_addr = footer_align.forward(aligned_addr + data_len);
+        assert(footer_align.check(footer_addr));
+
+        return @ptrFromInt(footer_addr);
+    }
+
+    // Writes the footer at the correct position for if it has moved
+    fn writeAcquired(self: Footer, data_ptr: [*]u8, data_len: usize) void {
+        acquire(data_ptr, data_len).* = self;
+    }
+
+    // Calculates the length that the allocation must be to
+    // store and align both the memory and the footer
+    fn overAllocLength(data_len: usize, data_align: Alignment) usize {
+        const map_len = data_len + @sizeOf(Footer);
+        return map_len + footer_align.toByteUnits() + data_align.toByteUnits();
+    }
+
+    // Calculates the length of the memory mapped region and returns a slice to it.
+    // This works because the data length is always tied to the mapped length.
+    fn mappedSlice(
+        self: Footer,
+        data_len: usize,
+        data_align: Alignment,
+    ) []align(std.heap.page_size_min) u8 {
+        return self.origin[0..overAllocLength(data_len, data_align)];
+    }
+};
+
 fn alloc(_: *anyopaque, len: usize, a: Alignment, _: usize) ?[*]u8 {
     const fd = tmpfile() catch return null;
 
     // Allocate enough space for the footer & proper alignment
-    const map_len = len + @sizeOf(Footer);
-    const over_alloc = map_len + footer_a.toByteUnits() + a.toByteUnits();
+    const over_alloc = Footer.overAllocLength(len, a);
     std.posix.ftruncate(fd, @intCast(over_alloc)) catch {
         std.posix.close(fd);
         return null;
     };
 
-    // Ensure that the mapping will link to the file on disk, and not RAM
-    const mapping = std.posix.mmap(
-        null,
-        over_alloc,
-        std.posix.PROT.WRITE | std.posix.PROT.READ | std.posix.PROT.SEM,
-        .{ .UNINITIALIZED = true, .NORESERVE = true, .TYPE = .SHARED },
-        fd,
-        0,
-    ) catch {
+    // Allow our page to be readable, writable, and used for atomics
+    const prot: u32 = PROT.WRITE | PROT.READ | PROT.SEM;
+    // Our page must map directly to the temporary file on-disk
+    const map: MAP = .{ .UNINITIALIZED = true, .NORESERVE = true, .TYPE = .SHARED };
+
+    const mapping = std.posix.mmap(null, over_alloc, prot, map, fd, 0) catch {
         std.posix.close(fd);
         return null;
     };
 
-    // Align the footer and memory correctly
-    const aligned_addr = a.forward(@intFromPtr(mapping.ptr));
-    assert(a.check(aligned_addr));
-
-    const footer_addr = footer_a.forward(aligned_addr + len);
-    assert(footer_a.check(footer_addr));
-
     // Set the footer so that we can resize/remap/free later
-    const footer: *Footer = @ptrFromInt(footer_addr);
-    footer.* = .{
+    Footer.acquire(mapping.ptr, len).* = .{
         .origin = mapping.ptr,
-        .map_len = over_alloc,
-        .file_len = over_alloc,
         .fd = fd,
     };
 
-    return @ptrFromInt(aligned_addr);
+    // Align the memory correctly
+    const data_addr = a.forward(@intFromPtr(mapping.ptr));
+    assert(a.check(data_addr));
+
+    return @ptrFromInt(data_addr);
 }
 
-/// Attempt to expand or shrink memory in place.
-///
-/// `memory.len` must equal the length requested from the most recent
-/// successful call to `alloc`, `resize`, or `remap`. `alignment` must
-/// equal the same value that was passed as the `alignment` parameter to
-/// the original `alloc` call.
-///
-/// A result of `true` indicates the resize was successful and the
-/// allocation now has the same address but a size of `new_len`. `false`
-/// indicates the resize could not be completed without moving the
-/// allocation to a different address.
-///
-/// `new_len` must be greater than zero.
 fn resize(_: *anyopaque, memory: []u8, a: Alignment, new_len: usize, _: usize) bool {
-    const aligned_addr: usize = @intFromPtr(memory.ptr);
-    assert(a.check(aligned_addr));
-
-    const footer_addr = footer_a.forward(aligned_addr + memory.len);
-    assert(footer_a.check(footer_addr));
-
     // Allocate enough space for the footer & proper alignment
-    const new_map_len = new_len + @sizeOf(Footer);
-    const new_over_alloc = new_map_len + footer_a.toByteUnits() + a.toByteUnits();
+    const over_alloc = Footer.overAllocLength(new_len, a);
 
-    const old_footer_ptr: *Footer = @ptrFromInt(footer_addr);
-    const old_footer: Footer = old_footer_ptr.*;
-    const origin = old_footer.origin;
-    const fd = old_footer.fd;
+    // Copy the footer because it's pointer may be invalidated
+    const footer: Footer = Footer.acquire(memory.ptr, memory.len).*;
+    const mapped_len = footer.mappedSlice(memory.len, a).len;
 
     if (new_len > memory.len) {
-        // Attempt to increase the length of the backing file before we attempt to
-        // increase the mapped memory region. This is necessary in the case that the
-        // file truncation succeeds, but the mremap fails.
-        std.posix.ftruncate(fd, new_over_alloc) catch {
-            return false; // Unable to truncate the file
-        };
+        // We must attempt to grow the file before we remap, otherwise we
+        // will risk some bytes being mapped, but truncated from the file,
+        // resulting in a bus error each time we read this memory.
 
-        // We know that the file truncation succeeded, and this old footer is still
-        // in the right place as far as our code will still determine - update the
-        // file length to be in a stable state.
-        old_footer_ptr.file_len = new_over_alloc;
+        std.posix.ftruncate(footer.fd, over_alloc) catch return false;
 
         const mapping = std.posix.mremap(
-            origin,
-            old_footer.map_len,
-            new_over_alloc,
-            .{},
-            null,
-        ) catch {
-            // Unable to remap the file -
-            // We are in a stable state - the pointer remains the same, and because the
-            // backing file only increased in memory, we can read and write to the footer
-            // with the same address. After all, we find the address of the footer based
-            // on the length of the memory slice.
-            return false;
-        };
+            footer.origin,
+            mapped_len,
+            over_alloc,
+            .{ .FIXED = true },
+            footer.origin,
+        ) catch return false;
 
-        // The mapping position must not change
-        assert(mapping.ptr == origin);
-
-        const new_footer_addr = footer_a.forward(aligned_addr + new_len);
-        assert(footer_a.check(new_footer_addr));
-        const new_footer_ptr: *Footer = @ptrFromInt(new_footer_addr);
-
-        // Everything succeeded, update the metadata
-        new_footer_ptr.* = .{
-            .fd = fd,
-            .origin = origin,
-            .file_len = new_over_alloc,
-            .map_len = new_over_alloc,
-        };
+        assert(mapping.ptr == footer.origin);
     } else {
-        // Attempt to remap the memmory mapped region before we attempt to truncate the file
-        // This is because we will still be in a stable state where we can access the entire
-        // addressed range, and it will have a backing file for that entire range. In the
-        // case that the mremap succeeds but the ftruncate fails, we can still say that the
-        // resize was a success, but the metadata will help us track what we *actually* have
-        // in spare memory capacity for the file.
+        // We must attempt to remap before we shrink the file, otherwise we
+        // will risk some bytes being mapped, but truncated from the file,
+        // resulting in a bus error each time we read this memory.
+
         const mapping = std.posix.mremap(
-            origin,
-            old_footer.map_len,
-            new_over_alloc,
-            .{},
-            null,
-        ) catch {
-            return false; // Unable to remap the file
-        };
+            footer.origin,
+            mapped_len,
+            over_alloc,
+            .{ .FIXED = true },
+            footer.origin,
+        ) catch return false;
 
-        const new_footer_addr = footer_a.forward(aligned_addr + new_len);
-        assert(footer_a.check(new_footer_addr));
-        const new_footer_ptr: *Footer = @ptrFromInt(new_footer_addr);
+        assert(mapping.ptr == footer.origin);
 
-        // To ensure a stable state for the allocation, record the state of the allocation.
-        new_footer_ptr.* = .{
-            .fd = fd,
-            .origin = origin,
-            .file_len = old_footer.file_len,
-            .map_len = new_over_alloc,
-        };
-
-        // The mapping position must not change
-        assert(mapping.ptr == origin);
-
-        std.posix.ftruncate(fd, new_over_alloc) catch {
-            // Unable to truncate the file. Kinda unfortunate, this. Thankfully we are
-            // currently in a stable state as to simply return failure.
-            return false;
-        };
-
-        // Everything succeeded, update the metadata
-        new_footer_ptr.file_len = new_over_alloc;
+        // This is allowed to silently fail - further resizes will reattempt
+        // to correctly size the file anyways, and it doesn't effect our
+        // footer or memory mapping if it is a little large.
+        std.posix.ftruncate(footer.fd, over_alloc) catch {};
     }
+
+    // We have successfully resized our allocation, so now we need to update
+    // the footer for later resize, remap, or free calls.
+    footer.writeAcquired(footer.origin, new_len);
 
     return true;
 }
@@ -215,19 +165,11 @@ fn remap(_: *anyopaque, memory: []u8, a: Alignment, new_len: usize, ret_addr: us
     return null;
 }
 
-/// Free and invalidate a region of memory.
-///
-/// `memory.len` must equal the length requested from the most recent
-/// successful call to `alloc`, `resize`, or `remap`. `alignment` must
-/// equal the same value that was passed as the `alignment` parameter to
-/// the original `alloc` call.
-///
-/// `ret_addr` is optionally provided as the first return address of the
-/// allocation call stack. If the value is `0` it means no return address
-/// has been provided.
-fn free(_: *anyopaque, memory: []u8, a: Alignment, ret_addr: usize) void {
-    _ = .{ memory, a, ret_addr };
-    // well frick
+fn free(_: *anyopaque, data: []u8, data_align: Alignment, _: usize) void {
+    const footer: Footer = Footer.acquire(data.ptr, data.len).*;
+    const origin_slice = footer.mappedSlice(data.len, data_align);
+    std.posix.munmap(origin_slice);
+    std.posix.close(footer.fd);
 }
 
 fn tmpfile() !std.posix.fd_t {
@@ -242,4 +184,12 @@ fn tmpfile() !std.posix.fd_t {
     errdefer std.posix.close(fd);
     try std.posix.unlink(path);
     return fd;
+}
+
+test "falloc" {
+    const gpa: Allocator = allocator;
+    try std.heap.testAllocator(gpa);
+    try std.heap.testAllocatorAligned(gpa);
+    try std.heap.testAllocatorAlignedShrink(gpa);
+    try std.heap.testAllocatorLargeAlignment(gpa);
 }
--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ const Falloc = @import("Falloc");
 pub fn main() !void {
     const gpa: Allocator = Falloc.allocator;
     const mem = try gpa.alloc(u8, 1 << 30); // 1 GiB
-    try std.heap.testAllocator(gpa);
+    defer gpa.free(mem);
     const hello = "Hello, World!\n";
     @memcpy(mem[0..hello.len], hello);
     std.debug.print("{s}", .{mem[0..hello.len]});
